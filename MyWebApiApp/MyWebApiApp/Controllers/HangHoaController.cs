6939b9407da3e8d371c5387ceee585b4cfe9dac5
Vĩnh Thuận <vinhthuan140201@gmail.com>
2022-10-14 19:28:45 +0700

    RestApi and Postman test
    Learn RestFull APi about get, post, delete, put. Testing API with Postman

---
+++ MyWebApiApp/MyWebApiApp/Controllers/HangHoaController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyWebApiApp.Models;
+
+namespace MyWebApiApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HangHoaController : ControllerBase
+    {
+        public static List<HangHoa> hangHoas = new List<HangHoa>();
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(hangHoas);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetById(string id)
+        {
+            try
+            {
+
+                var hangHoa = hangHoas.SingleOrDefault(hh=> hh.MaHangHoa == Guid.Parse(id));
+                if (hangHoa == null)
+                {
+                    return NotFound();
+                }
+                return Ok(hangHoa);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+        [HttpPost]
+        public IActionResult Create(HangHoaVM hanghoaVM)
+        {
+            var hanghoa = new HangHoa
+            {
+                MaHangHoa = Guid.NewGuid(),
+                TenHangHoa = hanghoaVM.TenHangHoa,
+                DonGia = hanghoaVM.DonGia
+            };
+            hangHoas.Add(hanghoa);
+            return Ok(new
+            {
+                Success = true, Data = hanghoa
+            });
+        }
+        [HttpPut("{id}")]
+        public IActionResult Edit(string id, HangHoa hanghoaEdit)
+        {
+            try
+            {
+                var hangHoa = hangHoas.SingleOrDefault(p => p.MaHangHoa == Guid.Parse(id));
+                if (hangHoa == null)
+                {
+                    return NotFound();
+                }
+                if (id != hangHoa.MaHangHoa.ToString())
+                {
+                    return BadRequest();
+                }
+                // update
+                hangHoa.TenHangHoa = hanghoaEdit.TenHangHoa;
+                hangHoa.DonGia = hanghoaEdit.DonGia;
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id, HangHoa hanghoaDelete)
+        {
+            try
+            {
+                var item = hangHoas.SingleOrDefault(p => p.MaHangHoa == Guid.Parse(id));
+                if (item == null)
+                {
+                    return NotFound();
+                }
+                hangHoas.Remove(item);
+                   return Ok();
+            }
+            
+            catch
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ MyWebApiApp/MyWebApiApp/Models/HangHoa.cs
@@ -0,0 +1,11 @@
+﻿namespace MyWebApiApp.Models
+{
+    public class HangHoaVM
+    {
+        public string TenHangHoa { get; set; }
+        public double DonGia { get; set; }
+    } public class HangHoa : HangHoaVM
+    {
+        public Guid MaHangHoa { get; set; }
+    }
+}
